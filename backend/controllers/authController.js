919cd08e9e5830698dd8f0487160dd4bc875ce5b
Bhumi Gupta <bg0674079@gmail.com>
2025-08-17 23:56:03 +0530

    bcry

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
 // Generate a random 4-digit OTP
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const AdUser = require('../models/AdUser');
 const bcrypt = require('bcryptjs');
+
 const jwt = require('jsonwebtoken');
 
 // Signup
